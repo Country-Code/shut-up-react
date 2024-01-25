d48f5cf243c40e7e1978b1c788b6f71e91fd511d
SoufianeMoutaouakkil <soufiane.moutaouakkil.ma@gmail.com>
2024-01-25 21:48:55 +0100

    setup typing event with useSocket

--- src/services/state/messages/messagesActions.js
+++ src/services/state/messages/messagesActions.js
@@ -1,6 +1,6 @@
-import * as actionsType from "./actionTypes";
-import * as chatsActionsType from "../chats/actionTypes";
-import * as socketActionsType from "../socket/actionTypes";
+import * as ActionTypes from "./actionTypes";
+import * as chatsActionTypes from "../chats/actionTypes";
+import * as socketActionTypes from "../socket/actionTypes";
 
 import api from "../../api/api";
 
@@ -8,77 +8,77 @@ export default (messagesApi) => {
     return {
         sendMessage: (chatId, content) => async (dispatch) => {
             try {
-                dispatch({ type: actionsType.MESSAGES_SEND_MESSAGE_REQUEST });
+                dispatch({ type: ActionTypes.MESSAGES_SEND_MESSAGE_REQUEST });
                 const data = await messagesApi.sendMessage(chatId, content);
                 dispatch({
-                    type: actionsType.MESSAGES_SEND_MESSAGE_SUCCESS,
+                    type: ActionTypes.MESSAGES_SEND_MESSAGE_SUCCESS,
                     payload: data,
                 });
                 dispatch({
-                    type: actionsType.MESSAGES_ADD_MESSAGE,
+                    type: ActionTypes.MESSAGES_ADD_MESSAGE,
                     payload: {id: chatId, message: data.message},
                 });
                 dispatch({
-                    type: chatsActionsType.CHATS_REFRESH_NEW_MESSAGE,
+                    type: chatsActionTypes.CHATS_REFRESH_NEW_MESSAGE,
                     payload: {id: chatId, message: data.message},
                 });
                 dispatch({
-                    type: socketActionsType.SOCKET_EMIT_NEW_MESSAGE,
+                    type: socketActionTypes.SOCKET_EMIT_NEW_MESSAGE,
                     payload: {message: data.message},
                 });
             } catch (error) {
                 api.dispatchError(
                     dispatch,
-                    actionsType.MESSAGES_SEND_MESSAGE_FAIL,
+                    ActionTypes.MESSAGES_SEND_MESSAGE_FAIL,
                     error
                 );
             }
         },
         getChatMessages: (chatId) => async (dispatch) => {
             try {
-                dispatch({ type: actionsType.MESSAGES_GET_CHAT_MESSAGES_REQUEST });
+                dispatch({ type: ActionTypes.MESSAGES_GET_CHAT_MESSAGES_REQUEST });
                 const data = await messagesApi.getChatMessages(chatId);
                 dispatch({
-                    type: actionsType.MESSAGES_GET_CHAT_MESSAGES_SUCCESS,
+                    type: ActionTypes.MESSAGES_GET_CHAT_MESSAGES_SUCCESS,
                     payload: data,
                 });
             } catch (error) {
                 api.dispatchError(
                     dispatch,
-                    actionsType.MESSAGES_GET_CHAT_MESSAGES_FAIL,
+                    ActionTypes.MESSAGES_GET_CHAT_MESSAGES_FAIL,
                     error
                 );
             }
         },
         readMessage: (messageId) => async (dispatch) => {
             try {
-                dispatch({ type: actionsType.MESSAGES_READ_MESSAGE_REQUEST });
+                dispatch({ type: ActionTypes.MESSAGES_READ_MESSAGE_REQUEST });
                 const data = await messagesApi.readMessage(messageId);
                 dispatch({
-                    type: actionsType.MESSAGES_READ_MESSAGE_SUCCESS,
+                    type: ActionTypes.MESSAGES_READ_MESSAGE_SUCCESS,
                     payload: data,
                 });
             } catch (error) {
                 api.dispatchError(
                     dispatch,
-                    actionsType.MESSAGES_READ_MESSAGE_FAIL,
+                    ActionTypes.MESSAGES_READ_MESSAGE_FAIL,
                     error
                 );
             }
         },
         fitMessagesList: (id, messagesList) => (dispatch) => {
             dispatch({
-                type: actionsType.MESSAGES_FIT_MESSAGES_LIST,
+                type: ActionTypes.MESSAGES_FIT_MESSAGES_LIST,
                 payload: {id, messagesList},
             });
         },
         addMessageToList: (id, message) => (dispatch) => {
             dispatch({
-                type: actionsType.MESSAGES_ADD_MESSAGE,
+                type: ActionTypes.MESSAGES_ADD_MESSAGE,
                 payload: {id, message},
             });
             dispatch({
-                type: chatsActionsType.CHATS_REFRESH_NEW_MESSAGE,
+                type: chatsActionTypes.CHATS_REFRESH_NEW_MESSAGE,
                 payload: {id, message},
             });
         },
