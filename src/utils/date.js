f9070989b045eba62e3ab28d18e1f313d6a0eb22
SoufianeMoutaouakkil <soufiane.moutaouakkil.ma@gmail.com>
2024-01-29 15:31:41 +0100

    rewrite date for chat item in chatList and message

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,57 +1,47 @@
-const isToday = (date) => {
+const isToday = (dateObject) => {
     const today = new Date();
-
     return (
-        today.getFullYear() === date.getFullYear() &&
-        today.getMonth() === date.getMonth() &&
-        today.getDate() === date.getDate()
+        today.getFullYear() === dateObject.getFullYear() &&
+        today.getMonth() === dateObject.getMonth() &&
+        today.getDate() === dateObject.getDate()
     );
+};
+
+const getFormatedTime = (dateObject) => {
+    return `${dateObject.getHours().toString().padStart(2, "0")}:${dateObject
+        .getMinutes()
+        .toString()
+        .padStart(2, "0")}`;
+};
+
+const getFormatedDate = (dateObject) => {
+    let formattedDateMonth = (dateObject.getMonth() + 1)
+        .toString()
+        .padStart(2, "0");
+    let formatedDateDay = dateObject.getDate().toString().padStart(2, "0");
+    return `${formatedDateDay}/${formattedDateMonth}`;
 };
 
 export default {
     getConversationDate: (date) => {
         const originalDate = new Date(date);
+        let formattedDate = "";
+        if (isNaN(originalDate)) return formattedDate;
 
-        const formattedDate = `${originalDate.getFullYear()}-${(
-            originalDate.getMonth() + 1
-        )
-            .toString()
-            .padStart(2, "0")}-${originalDate
-            .getDate()
-            .toString()
-            .padStart(2, "0")} ${originalDate
-            .getHours()
-            .toString()
-            .padStart(2, "0")}:${originalDate
-            .getMinutes()
-            .toString()
-            .padStart(2, "0")}`;
-
-        return formattedDate;
+        if (isToday(originalDate)) {
+            return getFormatedTime(originalDate);
+        }
+        return getFormatedDate(originalDate);
     },
     getMessageDate: (date) => {
         const originalDate = new Date(date);
         let formattedDate = "";
         if (isNaN(originalDate)) return formattedDate;
 
-        let formattedDateTime = `${originalDate
-            .getHours()
-            .toString()
-            .padStart(2, "0")}:${originalDate
-            .getMinutes()
-            .toString()
-            .padStart(2, "0")}`;
         if (isToday(originalDate)) {
-            formattedDate = formattedDateTime;
+            formattedDate = getFormatedTime(originalDate);
         } else {
-            let formattedDateMonth = (originalDate.getMonth() + 1)
-                .toString()
-                .padStart(2, "0");
-            let formattedDateDay = originalDate
-                .getDate()
-                .toString()
-                .padStart(2, "0");
-            formattedDate = `${formattedDateMonth}/${formattedDateDay} ${formattedDateTime}`;
+            formattedDate = `${getFormatedDate(originalDate)} ${getFormatedTime(originalDate)}`;
         }
         return formattedDate;
     },
